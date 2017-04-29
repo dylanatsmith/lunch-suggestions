4635d23cb42121a632661221472b95809ef58499
Dylan Smith <dylanatsmith@gmail.com>
2017-04-29 13:19:01 +0100

    Remove selection from array to prevent duplicates

--- assets/js/lunch.js
+++ assets/js/lunch.js
@@ -43,23 +43,35 @@ var restaurantsList = [
     website : 'http://www.the-white-hart.co.uk/home'
   }
 ];
-
+// Show time
 $(document).ready(function() {
+  // Prototype to remove object from array, removes first matching object only
+	Array.prototype.remove = function (v) {
+    if (this.indexOf(v) != -1) {
+      this.splice(this.indexOf(v), 1);
+      return true;
+    }
+    return false;
+	}
 	// Randomly select restaurant
 	var getRestaurant = function(restaurants) {
 		var choice = Math.floor(Math.random() * restaurants.length);
 	    return restaurants[choice];
 	}
 	var decision = getRestaurant(restaurantsList);
+	// Remove selected restaurant from list
+	restaurantsList.remove(decision);
+	console.log(restaurantsList);
 	// Delay new restaurant message
   setTimeout(function(){
   	// Add new restaurant message
 		  $(".conversation").append( "<div class='response'><a href='" + decision.website + "' class='response__text' target='_blank'><p>" + decision.name + "</p><span class='response__arrow'>&nbsp;⟶</span></a></div>" );
     // Force scroll to bottom of page
 	  window.scrollTo(0,document.body.scrollHeight);
-	// Set delay length
+	// Set message delay
 	}, 1000);
   $('#somewhere-else').click(function() {
+  	// Check that there are restaurants left
   	if (restaurantsList.length > 0) {
 	    // Add new call message bubble
 		  $(".conversation").append("<div class='call'><p class='call__text'>Somewhere else.</p></div>");
@@ -71,18 +83,31 @@ $(document).ready(function() {
 			    return restaurants[choice];
 			}
 			var decision = getRestaurant(restaurantsList);
+			// Remove selected restaurant from list
+			restaurantsList.remove(decision);
+			console.log(restaurantsList);
 			// Delay new restaurant message
 		  setTimeout(function(){
 		  	// Add new restaurant message
 			  $(".conversation").append( "<div class='response'><a href='" + decision.website + "' class='response__text' target='_blank'><p>" + decision.name + "</p><span class='response__arrow'>&nbsp;⟶</span></a></div>" );
 	      // Force scroll to bottom of page
 			  window.scrollTo(0,document.body.scrollHeight);
-			// Set delay length
+			// Set message delay
 			}, 750);
 		} else {
-			// Add end-of-the-line message
-		  $(".conversation").append("<div class='response'><p>I'm out of ideas.</p></div>");
-		  $("#somewhere-else").replaceWith('<p class="text-input__option">&nbsp;</p>');
+			// Add new call message bubble
+		  $(".conversation").append("<div class='call'><p class='call__text'>Somewhere else.</p></div>");
+		  // Force scroll to bottom of page
+			window.scrollTo(0,document.body.scrollHeight);
+			setTimeout(function(){
+			  // Add end-of-the-line message
+			  $(".conversation").append("<div class='response'><p class='response__text'>I'm out of ideas.</p></div>");
+			  // Remove option to prompt new response
+			  $("#somewhere-else").replaceWith('<p class="text-input__option">&#xfeff;</p>');
+			  // Force scroll to bottom of page
+			  window.scrollTo(0,document.body.scrollHeight);
+			// Set message delay
+		  }, 750);
 		}
 	});
 });
